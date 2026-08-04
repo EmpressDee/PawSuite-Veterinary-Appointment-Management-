0b3a9e8164981f97d1093fa0fb25cd94b146c9db
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-04 17:18:49 -0400

    add getAllTHings controller

--- backend/controllers/petController.js
+++ backend/controllers/petController.js
@@ -1,11 +1,13 @@
 //logic for CRUD goes here
+
 import Pet from "./models/Pet.js";
 
-export const getAllPets = async (req,res) => {
+export const getAllPets = async (req,res,next) => {
     try{
         const pets = await Pet.find();
         res.status(200).json(pets);
-    }catch (e) {
-        response.status(500).json({Error: e.message});
+    }catch (err) {
+        next(err);
     }
 }
+
