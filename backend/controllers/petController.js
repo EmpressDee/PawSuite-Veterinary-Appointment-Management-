f0529f570345e58d7d52db578a0fb8a6cfb64200
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-05 11:24:59 -0400

    created getPetById controller

--- backend/controllers/petController.js
+++ backend/controllers/petController.js
@@ -2,6 +2,7 @@
 
 import Pet from "./models/Pet.js";
 
+//GET
 export const getAllPets = async (req,res,next) => {
     try{
         const pets = await Pet.find();
@@ -10,4 +11,20 @@ export const getAllPets = async (req,res,next) => {
         next(err);
     }
 }
+
+// GET id
+export const getPetById = async (req,res,next) => {
+    try {
+        const petId = await Pet.findById(req.params.id)
+        if (!pet) {
+            return res.status(404).json({ message: "Not Found"})
+        }
+        res.status(200).json(petId);
+    } catch (err) {
+        next(err);
+    }
+}
+
+//POST
+
 
