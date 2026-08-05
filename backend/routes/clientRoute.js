9aca6c313d33f76c0619c963e52eede502664c6d
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-05 12:05:51 -0400

    created client routes

---
+++ backend/routes/clientRoute.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getAllClients, getClientById, createClient, updateClient, deleteClient } from "../controllers/clientController.js";
+
+const router = express.Router();
+
+router.get("/", getAllClients);
+router.get("/:id", getClientById);
+router.post("/", createClient);
+router.patch("/:id", updateClient); 
+router.delete("/:id", deleteClient); 
+
+export default router;
