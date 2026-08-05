58b84108792800f1999e02bce0ac5d53cf4d1c9b
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-05 12:22:53 -0400

    created appt routes

--- backend/routes/appointmentRoute.js
+++ backend/routes/appointmentRoute.js
@@ -1,1 +1,12 @@
 import express from "express";
+import { getAllAppointments, getAppointmentById, createAppointment, updateAppointment, deleteAppointment } from "../controllers/appointmentController.js";
+
+const router = express.Router();
+
+router.get("/", getAllAppointments);
+router.get("/:id", getAppointmentById);
+router.post("/", createAppointment);
+router.patch("/:id", updateAppointment);
+router.delete("/:id", deleteAppointment);
+
+export default router;
