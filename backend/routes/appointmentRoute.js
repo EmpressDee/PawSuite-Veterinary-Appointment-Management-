b157c22f56ba98469214bb2bcdbc7ca9fa209f89
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-06 14:57:58 -0400

    fixed function names

--- backend/routes/appointmentRoute.js
+++ backend/routes/appointmentRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { getAllAppointments, getAppointmentById, createAppointment, updateAppointment, deleteAppointment } from "../controllers/appointmentController.js";
+import { getAllAppointments, getApptById, createAppt, updateAppt, deleteAppt } from "../controllers/appointmentController.js";
 
 const router = express.Router();
 
 router.get("/", getAllAppointments);
-router.get("/:id", getAppointmentById);
-router.post("/", createAppointment);
-router.patch("/:id", updateAppointment);
-router.delete("/:id", deleteAppointment);
+router.get("/:id", getApptById);
+router.post("/", createAppt);
+router.patch("/:id", updateAppt);
+router.delete("/:id", deleteAppt);
 
 export default router;
