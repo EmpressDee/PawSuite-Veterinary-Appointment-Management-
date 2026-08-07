48f264b955c248494913cbabe29f2d1f0bc1130f
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-07 11:49:25 -0400

    added useState

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,1 +1,24 @@
 // fetches current week view and search bar
+import {useState, useEffect} from "react";
+import {getAppointments} from "../api/appointments.js";
+import SearchBar from "../components/SearchBar.jsx";
+import {Link} from "react-router";
+
+
+function HomePage () {
+    const [appointments, setAppointments] = useState([]);
+    const [filterText, setFilterText] = useState(""),
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+
+    })
+    .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+    });
+}, []);
+
+
+export default HomePage
