2002462fa855799a23ddd0e38dd502b343cc6b01
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-09 22:30:16 -0400

    changed import Link to NavLink

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,13 @@
-import {Link} from "react-router";
+import {NavLink} from "react-router";
+import "./NavBar.css";
 
 export default function NavBar() {
     return (
-        <nav style={{ display: "flex", gap: "16px"}}>
-            <Link to="/">Home</Link>
-            <Link to="/clients">Clients</Link>
-            <Link to="/pets">Pets</Link>
+        <nav className="side-bar">
+            <p className="sidebar-title">🐾PawSuite</p>
+            <NavLink className="nav-link" to="/">Schedule</NavLink>
+            <NavLink className="nav-link" to="/clients">Clients</NavLink>
+            <NavLink className="nav-link" to="/pets">Pets</NavLink>
         </nav>
     );
 }
