5c918726fee5fca1227660be244649b803a35396
Dena Daniel <cjdaniel0530@gmail.com>
2026-09-04 23:17:46 -0400

    wrapped routes in ProtectedRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,11 @@ import Clients from "./pages/clientPage";
 import Pets from "./pages/petPage";
 import NavBar from "./components/NavBar";
 import AppointmentDetail from "./pages/ApptDetail";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ProtectedRoute from "./components/ProtectedRoute";
+import ApptCalendar from "./components/Calendar";
+
 
 export default function App() {
   return (
@@ -12,10 +17,13 @@ export default function App() {
       <NavBar />
       <div className="main-content">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/appointments/:id" element={<AppointmentDetail />} />
-          <Route path="/clients" element={<Clients />} />
-          <Route path="/pets" element={<Pets />} />
+          <Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+          <Route path="/appointments/:id" element={<ProtectedRoute><AppointmentDetail /></ProtectedRoute> } />
+          <Route path="/clients" element={ <ProtectedRoute><Clients /></ProtectedRoute> } />
+          <Route path="/pets" element={ <ProtectedRoute><Pets /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/calendar" element={<ProtectedRoute><ApptCalendar/></ProtectedRoute>}/>
         </Routes>
       </div>
     </div>
