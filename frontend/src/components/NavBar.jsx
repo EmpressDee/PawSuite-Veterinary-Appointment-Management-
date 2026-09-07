c72844f54f14ece4cb6cf401cd355b33cc6f0d0c
Dena Daniel <cjdaniel0530@gmail.com>
2026-09-06 23:07:37 -0400

    changed logo

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import {NavLink} from "react-router";
 import "./NavBar.css";
 import { CalendarDays, UsersRound, PawPrint, LogIn } from "lucide-react";
-
+import PawLogo from "../pages/PawLogo"
 export default function NavBar() {
     return (
         <nav className="side-bar">
-            <p className="sidebar-title">🐾PawSuite</p>
+            <PawLogo />
+            {/* <p className="sidebar-title">🐾PawSuite</p> */}
             <NavLink className="nav-link" to="/"><CalendarDays size={20} /> Appointments</NavLink>
             <NavLink className="nav-link" to="/clients"><UsersRound size={20} /> Clients</NavLink>
             <NavLink className="nav-link" to="/pets"><PawPrint size={20} /> Pets</NavLink>
