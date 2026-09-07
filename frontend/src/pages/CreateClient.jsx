79f535bc0dd6e07829147fbeaea6dc6bf00d60f3
Dena Daniel <cjdaniel0530@gmail.com>
2026-09-07 00:31:46 -0400

    clientForm input and Submit button

--- frontend/src/pages/CreateClient.jsx
+++ frontend/src/pages/CreateClient.jsx
@@ -1,26 +1,53 @@
-import {useState} from "react";
+import { useState } from "react";
 
+export default function CreateClient() {
+  const [clients, setClients] = useState([]);
+  const [clientForm, setClientForm] = useState({});
 
-export default function CreateClient ()  {
-    const [clients, setClients] = useState([]);
-    const [clientForm, setClientForm] = useState({});
-    
+  function handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
 
-     function handleChange(e) {
-        const name = e.target.name;
-        const value = e.target.value;
+    setClientForm({
+      ...clientForm, //spread operator. copies existing clientForm
+      [name]: value,
+    });
+  }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="firstName"
+        placeholder="First Name"
+        onChange={handleChange}
+      />
 
-        setClientForm({
-        ...clientForm, //spread operator. copies existing clientForm
-        [name]: value
-     });
+      <input
+        type="text"
+        name="lastName"
+        placeholder="Last Name"
+        onChange={handleChange}
+      />
 
+      <input 
+      type="text"
+      name="phone"
+      placeholder="xxx-xxx-xxxx"
+      onChange={handleChange}
+      />
 
-     }
-     
+      <input 
+      type="email"
+      name="email"
+      placeholder="email@address.com"
+      onChange={handleChange}
+       />
 
- return (
-     
- )
-};
+       <button>Submit</button>
+    </form>
+  );
+}
